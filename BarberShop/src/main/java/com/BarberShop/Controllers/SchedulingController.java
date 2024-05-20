e077cca1d49a7f55d60b1524efe4c5055df9da54
lhchoinski <lhchoinski04@gmail.com>
2024-05-20 17:40:56 -0300

    Create Scheduling Service and Update Controller

--- BarberShop/src/main/java/com/BarberShop/Controllers/SchedulingController.java
+++ BarberShop/src/main/java/com/BarberShop/Controllers/SchedulingController.java
@@ -1,12 +1,9 @@
 package com.BarberShop.Controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.UUID;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,86 +17,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.BarberShop.DTOs.Scheduling.SchedulingRequestDTO;
 import com.BarberShop.DTOs.Scheduling.SchedulingResponseDTO;
-import com.BarberShop.Entities.Scheduling;
-import com.BarberShop.Repositories.SchedulingRepository;
+import com.BarberShop.Services.SchedulingService;
 
 @RestController
 @RequestMapping("scheduling")
 public class SchedulingController {
 
     @Autowired
-    private SchedulingRepository repository;
+    private SchedulingService schedulingService;
 
-    // GetAll Schedulings
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public List<SchedulingResponseDTO> GetAll() {
-        List<SchedulingResponseDTO> schedulingList = repository.findAll().stream()
-                .map(SchedulingResponseDTO::new)
-                .collect(Collectors.toList());
-
-        return schedulingList;
+    public List<SchedulingResponseDTO> getAllSchedulings() {
+        return schedulingService.getAllSchedulings();
     }
 
-    // Get Scheduling By ID
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}")
     public ResponseEntity<SchedulingResponseDTO> getSchedulingById(@PathVariable UUID id) {
-        Optional<Scheduling> optionalScheduling = repository.findById(id);
-
-        if (optionalScheduling.isPresent()) {
-            Scheduling scheduling = optionalScheduling.get();
-            SchedulingResponseDTO schedulingResponseDTO = new SchedulingResponseDTO(scheduling);
-            return ResponseEntity.ok(schedulingResponseDTO);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return schedulingService.getSchedulingById(id);
     }
 
-    // Create Scheduling
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void SaveScheduling(@RequestBody SchedulingRequestDTO data) {
-
-        Scheduling schedulingData = new Scheduling(data);
-        repository.save(schedulingData);
-        return;
+    public ResponseEntity<?> saveScheduling(@RequestBody SchedulingRequestDTO data) {
+        return schedulingService.saveScheduling(data);
     }
 
-    // Update Scheduling
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PutMapping("/{id}")
     public ResponseEntity<?> updateScheduling(@PathVariable UUID id, @RequestBody SchedulingRequestDTO data) {
-        Optional<Scheduling> optionalScheduling = repository.findById(id);
-
-        if (optionalScheduling.isPresent()) {
-            Scheduling scheduling = optionalScheduling.get();
-
-            scheduling.setEmployee(data.employee());
-            scheduling.setCustomer(data.customer());
-            scheduling.setDate(data.date());
-
-            repository.save(scheduling);
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return schedulingService.updateScheduling(id, data);
     }
 
-    // Delete Scheduling
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteScheduling(@PathVariable UUID id) {
-        try {
-            if (repository.existsById(id)) {
-                repository.deleteById(id);
-                return ResponseEntity.ok().build();
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Erro ao deletar o customer: " + e.getMessage());
-        }
+        return schedulingService.deleteScheduling(id);
     }
 }
---
+++ BarberShop/src/main/java/com/BarberShop/Services/SchedulingService.java
@@ -0,0 +1,77 @@
+package com.BarberShop.Services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.BarberShop.DTOs.Scheduling.SchedulingRequestDTO;
+import com.BarberShop.DTOs.Scheduling.SchedulingResponseDTO;
+import com.BarberShop.Entities.Customer;
+import com.BarberShop.Entities.Employee;
+import com.BarberShop.Entities.Scheduling;
+import com.BarberShop.Repositories.SchedulingRepository;
+
+@Service
+public class SchedulingService {
+
+    @Autowired
+    private SchedulingRepository schedulingRepository;
+
+    public List<SchedulingResponseDTO> getAllSchedulings() {
+        List<Scheduling> schedulings = schedulingRepository.findAll();
+        return schedulings.stream()
+                .map(SchedulingResponseDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public ResponseEntity<SchedulingResponseDTO> getSchedulingById(UUID id) {
+        Optional<Scheduling> optionalScheduling = schedulingRepository.findById(id);
+        return optionalScheduling.map(scheduling -> ResponseEntity.ok(new SchedulingResponseDTO(scheduling)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<?> saveScheduling(SchedulingRequestDTO data) {
+        try {
+            Scheduling scheduling = new Scheduling(data);
+            schedulingRepository.save(scheduling);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao salvar o agendamento: " + e.getMessage());
+        }
+    }
+
+    public ResponseEntity<?> updateScheduling(UUID id, SchedulingRequestDTO data) {
+        Optional<Scheduling> optionalScheduling = schedulingRepository.findById(id);
+        if (optionalScheduling.isPresent()) {
+            Scheduling scheduling = optionalScheduling.get();
+            scheduling.setEmployee(data.employee());
+            scheduling.setCustomer(data.customer());
+            scheduling.setDate(data.date());
+            schedulingRepository.save(scheduling);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<?> deleteScheduling(UUID id) {
+        try {
+            if (schedulingRepository.existsById(id)) {
+                schedulingRepository.deleteById(id);
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao deletar o agendamento: " + e.getMessage());
+        }
+    }
+}
